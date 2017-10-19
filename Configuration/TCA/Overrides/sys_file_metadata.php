11d5d3017fa8aff4690a1d2b9c680460e8fed1f7
SvenJuergens <sj@nordsonne.de>
2017-10-19 12:15:45 +0200

    [CLEANUP] TCA Migration

--- Configuration/TCA/Overrides/sys_file_metadata.php
+++ Configuration/TCA/Overrides/sys_file_metadata.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die();
 $tmp_color_extractor_columns = [];
 
-foreach ([1,2,3,4,5] as $item){
+foreach ([1, 2, 3, 4, 5] as $item) {
     $tmp_color_extractor_columns['tx_colorextractor_color' . $item] = [
         'exclude' => true,
         'label' => 'LLL:EXT:color_extractor/Resources/Private/Language/locallang_db.xlf:tx_colorextractor_domain_model_sysfilemetadata.tx_colorextractor_color' . $item,
@@ -10,20 +10,17 @@ foreach ([1,2,3,4,5] as $item){
             'type' => 'input',
             'size' => 6,
             'eval' => 'trim',
-            'wizards' => [
-                'colorChoice' => [
-                    'type' => 'colorbox',
-                    'module' => [
-                        'name' => 'wizard_colorpicker',
-                    ],
-                ]
-            ]
+            'renderType' => 'colorpicker'
         ],
     ];
 }
 
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('sys_file_metadata',$tmp_color_extractor_columns);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'sys_file_metadata',
+    $tmp_color_extractor_columns
+);
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
     'sys_file_metadata',
     'tx_colorextractor_color1,tx_colorextractor_color2,tx_colorextractor_color3,tx_colorextractor_color4,tx_colorextractor_color5',
